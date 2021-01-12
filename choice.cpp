82444075215d2b0d85c1239a49f6b7d31f9f200e
Daniyal_Akram_07 <77106224+daniyal-akram@users.noreply.github.com>
2021-01-12 11:43:14 +0500

    Add files via upload

---
+++ choice.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int x,y,sum,prod,sub,div;
+  cout<<"Entre the First value=";
+  cin>>x;
+  cout<<"Entre the Second value=";
+  cin>>y;
+  cout<<"sum=";
+  cout<<x+y<<endl;
+  cout<<"prod=";
+  cout<<x*y<<endl;
+  cout<<"sub=";
+  cout<<x-y<<endl;
+  cout<<"div=";
+  cout<<x/y<<endl; 
+}
